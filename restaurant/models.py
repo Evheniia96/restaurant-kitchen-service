7174cd9263fa1dc0107497c376848e4bc4e7d590
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-01-31 14:40:34 +0200

    add models in  empty app restaurant

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("restaurant:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="cooks")
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, dish type: {self.dish_type.name})"
--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "restaurant.Cook"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
