712c6f31ffa4503aa6e7c94db5d80a8df987bab7
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-02-07 23:21:10 +0200

    create test_form.py in the tests

---
+++ tests/test_forms.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from restaurant.forms import (CookCreationForm, CookSearchForm, DishTypeSearchForm, DishSearchForm)
+
+
+class CookCreationFormTest(TestCase):
+    def test_cook_creation_form_is_valid(self):
+        form_date = {
+            "username": "test",
+            "password1": "testPASSWORD",
+            "password2": "testPASSWORD",
+            "year_of_experience": 5,
+            "first_name": "test_first",
+            "last_name": "test_last"
+        }
+        form = CookCreationForm(data=form_date)
+        self.assertTrue(form.is_valid())
+        self.assertEquals(form.cleaned_data, form_date)
+
+
+class CookSearchFormTest(TestCase):
+    def test_cook_search_form_is_valid(self):
+        form_date = {"username": "test"}
+        form = CookSearchForm(data=form_date)
+        self.assertTrue(form.is_valid())
+
+
+class DishTypeSearchFormTest(TestCase):
+    def test_dish_type_search_form_is_valid(self):
+        form_date = {"name": "test_name_dish_type"}
+        form = DishTypeSearchForm(data=form_date)
+        self.assertTrue(form.is_valid())
+
+
+class DishSearchFormTest(TestCase):
+    def test_car_search_form_is_valid(self):
+        form_date = {"name": "test_name_dish"}
+        form = DishSearchForm(data=form_date)
+        self.assertTrue(form.is_valid())
--- tests/test_view/test_dish_type_views.py
+++ tests/test_view/test_dish_type_views.py
@@ -2,10 +2,10 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from restaurant.models import DishType
 
 DISH_TYPE_URL = reverse("restaurant:dish-type-list")
-DISH_URL = reverse("restaurant:dish-list")
-COOK_URL = reverse("restaurant:cook-list")
+DISH_TYPE_CREATE_URL= reverse("restaurant:dish-type-create")
 
 
 class PublicDishTypeTests(TestCase):
@@ -14,11 +14,27 @@ class PublicDishTypeTests(TestCase):
 
     def test_login_required(self):
         dish_type = self.client.get(DISH_TYPE_URL)
-        dish = self.client.get(DISH_URL)
-        cook = self.client.get(COOK_URL)
         self.assertNotEqual(dish_type.status_code, 200)
-        self.assertNotEqual(dish.status_code, 200)
-        self.assertNotEqual(cook.status_code, 200)
+
+    def test_create_login_required(self):
+        dish_type = self.client.get(DISH_TYPE_CREATE_URL)
+        self.assertNotEqual(dish_type.status_code, 200)
+
+    def test_update_delete_login_required(self):
+        DishType.objects.create(
+            name="Test_name"
+        )
+        dish_type = self.client.get(reverse(
+            "restaurant:dish_type-update",
+            kwargs={"pk": 1}
+        ))
+        self.assertNotEqual(dish_type.status_code, 200)
+
+        dish_type = self.client.get(reverse(
+            "restaurant:dish-type-delete",
+            kwargs={"pk": 1}
+        ))
+        self.assertNotEqual(dish_type.status_code, 200)
 
 
 class PrivateDishTypeTests(TestCase):
@@ -29,7 +45,20 @@ class PrivateDishTypeTests(TestCase):
         )
         self.client.force_login(self.user)
 
+    def test_retrieve_dish_types(self):
+        DishType.objects.create(name="Pasta")
+        DishType.objects.create(name="Pizza")
+        response = self.client.get(DISH_TYPE_URL)
+        self.assertEqual(response.status_code, 200)
+        dish_type = DishType.objects.all()
+        self.assertEqual(
+            list(response.context["dish_type_list"]),
+            list(dish_type)
+        )
+        self.assertTemplateUsed(response, "restaurant/dish_type_list.html")
 
-class PublicDishTests(TestCase):
-    def setUp(self) -> None:
-        self.client = Client()
+    def test_search_dish_types(self):
+        DishType.objects.create(name="Pizza")
+        response = self.client.get(DISH_TYPE_URL + "?name=Pizza")
+        self.assertEqual(response.status_code, 200)
+
