e54faf6599a8dee604c4f5915c202b496c5f2021
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-02-10 21:30:10 +0200

    update index.html with use bootstrap
    -add bootstrap templates
    -change index.html
    -add new img for Home page
    -install crispy-forms
    -change navigation.html as sitebar
    -change footer.html

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "restaurant",
 ]
 
@@ -69,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "restaurant_service.wsgi.application"
 
